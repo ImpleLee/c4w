fe4d957e0c9f4afd09b6de34e9776bd8876266b5
Imple Lee <80144331+ImpleLee@users.noreply.github.com>
2024-02-23 14:13:15 +0800

    simplify some network flow works

--- src/prover/mod.rs
+++ src/prover/mod.rs
@@ -23,20 +23,23 @@ impl Next {
   fn is_geq(&self, other: &Self, branch_geq: impl Fn(usize, usize) -> bool) -> bool {
     let left = self.0.clone();
     let right = other.0.clone();
-    let mut graph_builder = VecGraphBuilder::<u8>::with_capacities(
+    let mut graph_builder = VecGraphBuilder::<usize>::with_capacities(
       2 + left.len() + right.len(),
       (1 + left.len()) * (1 + right.len()) - 1
     );
     let left_node = graph_builder.add_node();
     let left_branches = graph_builder.add_nodes(left.len());
-    let left_edges = left_branches.iter()
-      .map(|&left_branch| graph_builder.add_edge(left_node, left_branch))
-      .collect::<HashSet<_>>();
+    use rs_graph::traits::Indexable;
+    for &left_branch in left_branches.iter() {
+      let edge = graph_builder.add_edge(left_node, left_branch);
+      assert!(edge.index() < left.len());
+    }
     let right_node = graph_builder.add_node();
     let right_branches = graph_builder.add_nodes(right.len());
-    let right_edges = right_branches.iter()
-      .map(|&right_branch| graph_builder.add_edge(right_branch, right_node))
-      .collect::<HashSet<_>>();
+    for &right_branch in right_branches.iter() {
+      let edge = graph_builder.add_edge(right_branch, right_node);
+      assert!(edge.index() >= left.len() && edge.index() < left.len() + right.len());
+    }
     for (&left_id, &left_branch) in left.iter().zip(left_branches.iter()) {
       for (&right_id, &right_branch) in right.iter().zip(right_branches.iter()) {
         if branch_geq(left_id, right_id) {
@@ -46,18 +49,16 @@ impl Next {
     }
     let graph = graph_builder.into_graph();
     let mut push_relabel = PushRelabel::new(&graph);
-    let left_len = left.len() as u8;
-    let right_len = right.len() as u8;
     push_relabel.solve(left_node, right_node, |e| {
-      if left_edges.contains(&e) {
-        right_len
-      } else if right_edges.contains(&e) {
-        left_len
+      if e.index() < left.len() {
+        right.len()
+      } else if e.index() < left.len() + right.len() {
+        left.len()
       } else {
-        u8::MAX
+        usize::MAX
       }
     });
-    push_relabel.value() == left_len * right_len
+    push_relabel.value() == left.len() * right.len()
   }
 }
 
